c5e4a2185a3b44a4d28197610c6c0f84ed7b698c
Petra Vidnerova <petra.vidnerova@gmail.com>
2025-10-19 20:07:24 +0200

    also parquet

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,5 +3,7 @@ import pandas as pd
 def read_df(name, *args, **kwargs):
     if name.endswith(".feather"):
         return pd.read_feather(name)
+    elif name.endswith(".parquet"):
+        return pd.read_parquet(name)
     else:
         return pd.read_csv(name, **kwargs)
