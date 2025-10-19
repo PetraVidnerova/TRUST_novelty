c89e19b67c2143c1b8dc1566fa1f4380bd7a8490
Petra Vidnerova <petra.vidnerova@gmail.com>
2025-10-19 20:14:05 +0000

    logs

--- src/specter2_novelty/calc_lof.py
+++ src/specter2_novelty/calc_lof.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import tqdm
 from sklearn.neighbors import LocalOutlierFactor
 from sklearn.ensemble import IsolationForest
 import torch
@@ -19,9 +20,10 @@ else:
 
 rowids = [] 
 docvecs = []
-for filename in os.listdir(EMBDIR):
-    if not filename.startswith(settings.output_prefix):
-        continue
+print("Loading docvecs ... ")
+files = [f for f in list(os.listdir(EMBDIR)) if f.startswith(settings.output_prefix)] 
+
+for filename in tqdm.tqdm(files):
     rowid = filename[len(settings.output_prefix)+1:-3] # skip prefix + _ and '.pt'
     if settings.output_prefix == "arxiv_dataset":
         rowid = f"http://arxiv.org/abs/{rowid.replace(':','/')}"
@@ -31,20 +33,23 @@ for filename in os.listdir(EMBDIR):
 
 docvecs = np.vstack(docvecs)
 print(docvecs.shape)
-
+print("Docvecs prepared.")
 
 #
 # TODO!!!!
 # use train/reference set and testset 
 #
-
-clf = LocalOutlierFactor(n_neighbors=10)
+print("Fitting LocalOutlierFactor")
+clf = LocalOutlierFactor(n_neighbors=20)
 clf.fit(docvecs)
 novelty_lof = clf.negative_outlier_factor_ * -1
+print("Done")
 
+print("Fitting IsolationForest")
 clf = IsolationForest()
 clf.fit(docvecs)
 novelty_if = clf.score_samples(docvecs) * -1
+print("Done")
 
 ID = settings.id_column_name
 
@@ -56,5 +61,5 @@ result2 = pd.DataFrame()
 result2[ID] = rowids
 result2["score"] = novelty_if
 
-result1.to_csv(f"{RESULTS}/{settings.output_prefix}_specter2_LOF_result.csv")
+result1.to_csv(f"{RESULTS}/{settings.output_prefix}_specter2_LOF_n=20_result.csv")
 result2.to_csv(f"{RESULTS}/{settings.output_prefix}_specter2_IF_result.csv")
