df93ac7991e1b8fc0a16fb061db56e2cf6327385
Petra Vidnerova <petra.vidnerova@gmail.com>
2025-10-19 19:28:44 +0200

    ntb fixes

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def read_df(name):
+def read_df(name, *args, **kwargs):
     if name.endswith(".feather"):
         return pd.read_feather(name)
     else:
-        return pd.read_csv(name)
+        return pd.read_csv(name, **kwargs)
