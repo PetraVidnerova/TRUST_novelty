6fa2b6ab234dd996dd8c84cbd845e5c035c22982
Petra Vidnerova <petra.vidnerova@gmail.com>
2025-10-19 20:28:15 +0200

    cell

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,6 +4,6 @@ def read_df(name, *args, **kwargs):
     if name.endswith(".feather"):
         return pd.read_feather(name)
     elif name.endswith(".parquet"):
-        return pd.read_parquet(name)
+        return pd.read_parquet(name, engine="fastparquet")
     else:
         return pd.read_csv(name, **kwargs)
