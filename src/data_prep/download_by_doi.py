a920cc3f8d2099a3e31694a97ae79a9b7133f63d
Petra Vidnerova <petra.vidnerova@gmail.com>
2025-10-20 06:29:36 +0000

    doi2pdf for cell download

---
+++ src/data_prep/download_by_doi.py
@@ -0,0 +1,37 @@
+import os
+import time
+import tqdm 
+import doi2pdf
+from utils.utils import read_df
+
+with open("file_not_found.txt", "r") as f:
+    not_found_ids = f.readlines()
+    not_found_ids = list(map(lambda x: x.strip(), not_found_ids))
+    
+
+df = read_df("../../data/cell/cell_papers_selected_novelty.parquet")
+
+
+for i, row in tqdm.tqdm(df.iterrows(), total=len(df)):
+    if os.path.exists(f"../../data/cell/PDF/{i}.pdf"):
+        continue
+
+    doi = row["doi"]
+    if doi in not_found_ids:
+        continue
+    print(f"Processing {doi} ... ", end="", flush=True)
+    try:
+        doi2pdf.doi2pdf(doi, output=f"../../data/cell/PDF/{i}.pdf")
+    except doi2pdf.main.NotFoundError:
+        print("File not found, skipping.")
+        with open("file_not_found.txt", "a") as f:
+            print(doi, file=f)
+        continue
+    except:
+        print("Other error")
+        with open("file_not_found.txt", "a") as f:
+            print(doi, file=f)
+        continue
+        
+    print("saved", flush=True)
+    time.sleep(10)
