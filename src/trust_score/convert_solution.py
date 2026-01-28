fee715f970f4ab804ef8b37a16be058e9aee562d
Petra Vidnerova <petra.vidnerova@gmail.com>
2026-01-28 16:21:32 +0100

    attempt to analyze results

---
+++ src/trust_score/convert_solution.py
@@ -0,0 +1,24 @@
+import pickle
+import pandas as pd
+
+
+with open("tmp_result_cell.pkl", "rb") as f:
+    result = pickle.load(f)
+
+df = pd.DataFrame.from_dict(result, orient='index')
+df["PaperProjectID"] = df.index
+print(df)
+
+df_orig = pd.read_csv("../../data/soutez/cell.csv")
+print(df_orig)
+
+print(df.columns)
+print(df_orig.columns)
+
+df_merged = df.merge(df_orig, on="PaperProjectID", how="left", suffixes=('_eval', '_orig'))
+df_merged = df_merged[["PaperProjectID", "OpenAlexID (as URL)", "score"]]
+
+print(df_merged)
+print(df_merged.columns)
+
+df_merged.to_csv("cell_eval_results.csv", index=False)
